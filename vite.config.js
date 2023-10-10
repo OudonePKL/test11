aaac934be85af19538a3dea6d7389063a4bf6c46
seonekr <oudonephengkhamlar@gmail.com>
2023-10-10 11:06:22 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/OudonePKL/test11",
+  base: "https://.github.io/OudonePKL/test11",
 });
